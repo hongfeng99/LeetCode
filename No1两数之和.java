ce62d9b3f9162171921c88c1eb4c2f3d0d727ae4
hongfeng99 <2781888293@qq.com>
2024-11-18 14:40:42 +0800

    Add files via upload

---
+++ No1两数之和.java
@@ -0,0 +1,22 @@
+package 力扣超级版本;
+
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+        	map.put(nums[i],i);
+        }
+        for(int i=0;i<n;i++)
+        {
+        	if(map.containsKey(target-nums[i])&&map.get(target-nums[i])!=i)
+        	{
+        		return new int[] {i,map.get(target-nums[i])};
+        	}
+        }
+        return new int[] {-1,-1};
+    }
+}
