45c8ae10ff26756551e6309a4efaad8120b439e9
hongfeng99 <2781888293@qq.com>
2024-11-18 21:49:13 +0800

    Update 6. Z 字形变换.java

--- 6. Z 字形变换.java
+++ 6. Z 字形变换.java
@@ -39,3 +39,49 @@ class Solution {
     	return str.toString();
     }
 }
+//重新做用的方法，图形化Z字形，把每一个的横纵坐标位置都用数学方法找到
+package 力扣超级版本;
+
+class Solution {
+    public String convert(String s, int numRows) {
+        int num=s.length();
+        int m=numRows;
+        if(m==1)
+        {
+            return s;
+        }
+        int count=num/(2*m-2)+1;//全部的组数
+        int n=count*(m-1);
+        char[][] dp=new char[m][n];
+        for(int i=0;i<num;i++)
+        {
+        	int x=0;
+        	int y=0;
+        	int rank0=(i/(2*m-2)); //rank0是第几组
+        	int rank1=(i%(2*m-2)); //rank1是组内的次序
+        	if(rank1<m)
+        	{
+        		y=(m-1)*rank0;
+        		x=rank1;
+        	}
+        	else
+        	{
+        		x=2*m-rank1-2;
+        		y=(m-1)*rank0+rank1-m+1;
+        	}
+        	dp[x][y]=s.charAt(i);
+        }
+        StringBuffer ans=new StringBuffer();
+        for(int i=0;i<m;i++)
+        {
+        	for(int j=0;j<n;j++)
+        	{
+        		if(dp[i][j]!='\0')
+        		{
+        			ans.append(dp[i][j]);
+        		}
+        	}
+        }
+        return ans.toString();
+    }
+}
