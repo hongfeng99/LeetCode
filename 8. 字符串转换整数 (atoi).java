42f400ea67a3f1e18ce1e6b35a55ef12ec6af6bc
hongfeng99 <2781888293@qq.com>
2024-11-19 15:00:22 +0800

    Update 8. 字符串转换整数 (atoi).java
    已经Debug

--- 8. 字符串转换整数 (atoi).java
+++ 8. 字符串转换整数 (atoi).java
@@ -102,3 +102,113 @@ class Solution {
         return 0;
     }
 }
+/////////////////////////////////////////////////////////////////已经DEbug
+class Solution {
+    public int myAtoi(String s) {
+        int n=s.length();
+        //如果为空字符串
+        if(n==0)
+        {
+        	return 0;
+        }
+        //定义一个从头开始遍历的指针
+        int ptr=0;
+        //读取并且跨过所有空格
+        while(ptr<n&&s.charAt(ptr)==' ')
+        {
+        	ptr++;
+        }
+        //如果整个字符串都是空格
+        if(ptr==n)
+        {
+        	return 0;
+        }
+        //如果去除所有前置空格得到的是'-'
+        if(s.charAt(ptr)=='-')
+        {
+        	//如果以'-'结尾或者'-'后面紧跟的第一个字符不是数字
+        	if(ptr==n-1||s.charAt(ptr+1)<'0'||s.charAt(ptr+1)>'9')
+        	{
+        		return 0;
+        	}
+        	ptr++;
+        	//读取所有的0并且跨过
+        	while(ptr<n&&s.charAt(ptr)=='0')
+        	{
+        		ptr++;
+        	}
+        	if(ptr==n)
+        	{
+        		return 0;
+        	}
+        	else
+        	{
+        		long sum=0;
+        		while(ptr<n&&s.charAt(ptr)>='0'&&s.charAt(ptr)<='9')
+        		{
+        			sum*=10;
+        			sum+=s.charAt(ptr)-'0';
+        			if(sum*(-1)<Integer.MIN_VALUE)
+            		{
+            			return Integer.MIN_VALUE;
+            		}
+        			ptr++; 
+        		}
+        		sum*=(-1);
+        		
+        		return (int)sum;
+        	}
+        }
+        if(s.charAt(ptr)=='+')
+        {
+        	if(ptr==n-1||s.charAt(ptr+1)<'0'||s.charAt(ptr)>'9')
+        	{
+        		return 0;
+        	}
+        	else
+        	{
+        		ptr++;
+        		while(ptr<n&&s.charAt(ptr)=='0')
+        		{
+        			ptr++;
+        		}
+        		if(ptr==n)
+        		{
+        			return 0;
+        		}
+        		else
+        		{
+        			long sum=0;
+        			while(ptr<n&&s.charAt(ptr)>='0'&&s.charAt(ptr)<='9')
+        			{
+        				sum*=10;
+        				sum+=s.charAt(ptr)-'0';
+        				ptr++;
+        			}
+        			if(sum>Integer.MAX_VALUE)
+        			{
+        				return Integer.MAX_VALUE;
+        			}
+        			return (int)sum;
+        		}
+        	}
+        	
+        }
+        if(s.charAt(ptr)>='0'&&s.charAt(ptr)<='9')
+        {
+        	long sum=0;
+        	while(ptr<n&&s.charAt(ptr)>='0'&&s.charAt(ptr)<='9')
+        	{
+        		sum*=10;
+        		sum+=s.charAt(ptr)-'0';
+        		if(sum>Integer.MAX_VALUE)
+    			{
+    				return Integer.MAX_VALUE;
+    			}
+        		ptr++;
+        	}   	
+        	return (int)sum;
+        }
+        return 0;
+    }
+}
