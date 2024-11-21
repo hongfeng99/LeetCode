a1cefdf6b83924b1d6e7310ec77897b10ebf52bd
hongfeng99 <2781888293@qq.com>
2024-11-22 02:15:03 +0800

    Update 19. 删除链表的倒数第 N 个结点.java
    补充了一种方法

--- 19. 删除链表的倒数第 N 个结点.java
+++ 19. 删除链表的倒数第 N 个结点.java
@@ -34,3 +34,44 @@ class Solution {
     }
     
 }
+///////////////另一种用哈希表存储次序+链表节点的做法
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        Map<Integer,ListNode> map=new HashMap<>();
+        int count=0;
+        ListNode p=new ListNode(0);
+        p.next=head;
+        while(head!=null)
+        {
+            count++;
+        	map.put(count,head);        	
+        	head=head.next;
+        }
+        int target=count-n+1;
+        if(count-n+1==1)
+        {
+        	p.next=p.next.next;
+        	return p.next;
+        }
+        else
+        {
+        	map.get(count-n+1-1).next=map.get(count-n+1).next;
+        	return p.next;
+        }
+    }
+}
+
+
+
+
+
