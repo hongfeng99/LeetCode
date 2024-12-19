6c05046cd98abd67b5e67987ed05d755885b1393
hongfeng99 <2781888293@qq.com>
2024-12-19 15:12:57 +0800

    Update 55. 跳跃游戏.java

--- 55. 跳跃游戏.java
+++ 55. 跳跃游戏.java
@@ -31,3 +31,30 @@ class Solution {
         return true;
     }
 }
+/////////////////////////////////////////////////////////另外一种方法 枚举
+class Solution {
+    public boolean canJump(int[] nums) {
+      int n=nums.length;
+      int p=0;
+      int remost=0;
+      if(n==1)
+      {
+    	  return true;
+      }
+      while(p<n-1)
+      {
+    	  remost=Math.max(remost, nums[p]+p);
+    	  
+    	  if(nums[p]==0&&remost==p)
+    	  {
+    		  return false;
+    	  }
+    	  if(remost>=n-1)
+    	  {
+    		  return true;
+    	  }
+    	  p++;
+      }
+      return false;
+    }
+}
