bc3d1494c38e92d05fdc224163a774307b16fa8d
hongfeng99 <2781888293@qq.com>
2024-12-21 15:04:09 +0800

    Update 69. x 的平方根.java

--- 69. x 的平方根.java
+++ 69. x 的平方根.java
@@ -12,3 +12,17 @@ class Solution {
         return (int)--p;
     }
 }
+//更为高效的回答
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0)
+        {
+        	return 0;
+        }
+        else
+        {
+        	int ans=(int)Math.exp(0.5*Math.log(x))+1;
+        	return (long)ans*ans>x?ans-1:ans;
+        }
+    }
+}
