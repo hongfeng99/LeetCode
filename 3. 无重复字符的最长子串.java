4f874c7e08a9e54dc597318963bcc60fcdab64e5
hongfeng99 <2781888293@qq.com>
2025-03-04 15:46:42 +0800

    Update 3. 无重复字符的最长子串.java

--- 3. 无重复字符的最长子串.java
+++ 3. 无重复字符的最长子串.java
@@ -1,26 +1,27 @@
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n=s.length();
         if(n==0)
         {
-            return 0;
+        	return 0;
         }
-        int maxLen=1;
-        for(int i=0;i<n;i++)
-        {
-        	Set<Character>set=new HashSet<>();
-        	int j=i;
-        	while(j<n&&!set.contains(s.charAt(j)))
+        int low=0;
+        int high=0;
+        int ans=1;
+        Set<Character> set=new HashSet<>();
+        while(high<n)
+        {        	
+        	while(high<n&&!set.contains(s.charAt(high)))
         	{
-        		set.add(s.charAt(j));
-        		j++;
-                if(j<n&&set.contains(s.charAt(j)))
-                {
-                    break;
-                }
+        		set.add(s.charAt(high));
+        		high++;
         	}
-        	maxLen=Math.max(maxLen, j-i);
+        	ans=Math.max(ans, high-low);
+        	set.remove(s.charAt(low));
+        	low++;
         }
-        return maxLen;
+        return ans;
     }
 }
